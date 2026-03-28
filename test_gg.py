5e35a0e5bbdbc5898f261a2277fa8b3265a970e6
AltF4 <altf4petro@gmail.com>
2026-03-28 10:47:25 -0700

    Fix windows character encoding issue in tests

--- test_gg.py
+++ test_gg.py
@@ -3,7 +3,7 @@ import coco
 
 def test_sephiroth():
     # Read file into string
-    with open("test/GG - Sephiroth's Intervention [FINAL FANTASY].html", 'r') as file:
+    with open("test/GG - Sephiroth's Intervention [FINAL FANTASY].html", 'r', encoding='utf-8') as file:
         html_content = file.read()
         assert coco.gg.get_quantity_from_html(html_content) == 55
         
